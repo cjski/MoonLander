f1f89cf74ae1f62d9c9965d7ce9501a3c6731d5b
cjski <chrisfjski@gmail.com>
2017-08-14 22:46:24 -0400

    Update Ship.java
    Updated and added comments

--- moonlander/src/Ship.java
+++ moonlander/src/Ship.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
  */
 public class Ship {
 	
+	//Game variables
 	private int x;
 	private int y;
 	private int xspeed;
@@ -17,6 +18,11 @@ public class Ship {
 	private int fuel;
 	public String name;
 	
+	/*
+	 * Constructor, initializes all variables
+	 * parm x0 - inital x coordinate
+	 * parm y0 - intial y coordinate
+	 */
 	public Ship(int x0, int y0){
 		this.x = x0;
 		this.y = y0;
@@ -36,17 +42,31 @@ public class Ship {
 		sc.close();
 	}
 	
+	/*
+	 * Returns yspeed of the ship
+	 */
 	public int getYSpeed() {
 		return this.yspeed;
 	}
 	
+	/*
+	 * Returns fuel of the ship
+	 */
 	public int getFuel() {
 		return this.fuel;
 	}
+	
+	/*
+	 * Sets fuel of the ship
+	 * parm newFuel - new fuel level
+	 */
 	public void setFuel(int newFuel){
 		this.fuel = newFuel;
 	}
 	
+	/*
+	 * Sets boost of the ship
+	 */
 	public void setBoost(int boost){
 		this.boost = boost;
 	}
@@ -58,21 +78,37 @@ public class Ship {
 		return x;
 	}
 	
+	/*
+	 * Changes the angle the ship is aimed in
+	 * parm arg0 - key on the keyboard that has been pressed
+	 */
 	public void changeAngle(KeyEvent arg0) {
+		//If the user pressed left turn counterclockwise
 		if(arg0.getKeyCode() == KeyEvent.VK_LEFT)
 			this.angle += 10;
+		//if user pressed right turn clockwise
 		if(arg0.getKeyCode() == KeyEvent.VK_RIGHT)
 			this.angle += 10;
 		} 
 	
+	/*
+	 * Updates the speed based on gravity and the boost
+	 */
 	public void updatespeed() {
+			//If the angle is greater than 360 degrees, subtract 360 to ensure angle is between 0 and 359
 			while(Math.abs(this.angle) >=360 ) {
 				this.angle -= 360;
 			}
+		
+			//Update speeds
 			//this.xspeed += this.boost*Math.cos(this.angle);
+			//Yspeed is -= because positive is in the down direction
 			this.yspeed -= this.boost*Math.sin(this.angle) - 1;
 		}
-
+	
+	/*
+	 * Updates the position of the ship based on speed
+	 */
 	public void updatepos() {
 			this.x += this.xspeed;
 			this.y += this.yspeed;
